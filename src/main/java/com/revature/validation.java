247e7c97232d0b9963a351065abb4ab937647482
Training <srdssharmig97@gmail.com>
2019-08-14 01:33:34 -0400

    first commit

---
+++ src/main/java/com/revature/validation.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+public class validation {
+	
+	public static void validate(String name) {
+		
+		if(name=="sharmi")
+			System.out.println("valid name");
+		else
+			System.out.println("ïnvalid name");
+	}
+
+}
---
+++ src/test/java/com/revature/TestValidation.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+public class TestValidation {
+
+	public static void main(String[] args) {
+		//String name="sharmi";
+		validation.validate("sharmi");
+
+	}
+
+}
